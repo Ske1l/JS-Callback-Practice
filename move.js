8192c5e78a9b772812aab60eb7f74663afa21372
Ske1l <hayley6236@gmail.com>
2022-05-21 14:05:23 -0400

    I don't think nintendo signed off on this use of LoZ assets

--- move.js
+++ move.js
@@ -6,7 +6,58 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, callback) {
+        let direction = null;
+        let x = 100;
+        let y = 250;
+
+        setInterval(function () {
+            if (direction === 'west') {
+                x = x - 1
+            }
+            if (direction === 'north') {
+                y = y + 1
+            }
+            if (direction === 'east') {
+                x = x + 1
+            }
+            if (direction === 'south') {
+                y = y - 1
+            }
+            character.style.left = x + 'px'
+            character.style.bottom = y + 'px'
+        }, 1)
+
+        // setInterval(moveCharacter, 1) can be used with a moveCharacter funtion instead
+
+        document.addEventListener('keydown', function (e) {
+            if (e.repeat) return;
+
+            if (e.key === 'ArrowLeft') {
+                direction = 'west'
+            }
+            if (e.key === 'ArrowUp') {
+                direction = 'north'
+            }
+            if (e.key === 'ArrowRight') {
+                direction = 'east'
+            }
+            if (e.key === 'ArrowDown') {
+                direction = 'south'
+            }
+            callback(direction)
+        })/*activates once key is pressed down*/
+
+        document.addEventListener('keyup', function (e) {
+            direction = null
+            callback(direction)
+        }) /*defines what happens when key is let back up*/
+
+    }
+
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
